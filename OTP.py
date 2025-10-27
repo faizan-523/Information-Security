c708acf37265bc55801c93d78c4d534444ef44ea
faizan-523 <faizan.7.ist@gmail.com>
2025-10-27 22:14:36 +0500

    Cipher Technique

--- OTP.py
+++ OTP.py
@@ -1,3 +1,35 @@
 def OTP_Encryption(plaintext, key):
     plaintext = plaintext.upper()
     key = key.upper()
+    ciphertext = ""
+
+    for i in range(len(plaintext)):
+            p = ord(plaintext[i]) - 65
+            k = ord(key[i]) - 65
+            c = (p + k) % 26
+            ciphertext += chr(c + 65)
+    return ciphertext
+
+def OTP_Decryption(ciphertext, key):
+    ciphertext = ciphertext.upper()
+    key = key.upper()
+    plaintext = ""
+
+    for i in range(len(ciphertext)):
+            c = ord(ciphertext[i]) - 65
+            k = ord(key[i]) - 65
+            p = (c - k + 26) % 26
+            plaintext += chr(p + 65)
+    return plaintext
+
+pt = input("Enter Plaintext: ")
+k = input("Enter Key (same length as plaintext): ") 
+
+if len(pt) != len(k):
+    print("Error: Plaintext and Key must be of the same length!")
+else:
+    ct = OTP_Encryption(pt, k)
+    print("Ciphertext:", ct)
+
+    decrypted_pt = OTP_Decryption(ct, k)
+    print("Decrypted Plaintext:", decrypted_pt)
